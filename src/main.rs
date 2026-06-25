5409adbd4691e042dd2e08809ae30fcfd31970e3
gavril-s <gavril.senkevich@gmail.com>
2026-06-26 00:26:17 +0300

    Add axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("Listening on http://127.0.0.1:3000");
+
+    axum::serve(listener, app).await.unwrap();
 }
